fac1bcaf39bbeef0b71d57eecfd6de21e136627b
Shubham Phondani <phondanishubham23@gmail.com>
2024-08-23 13:34:29 +0530

    Time: 0 ms (100%), Space: 40.1 MB (81.82%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.java
+++ 0367-valid-perfect-square/0367-valid-perfect-square.java
@@ -2,22 +2,22 @@ class Solution {
     public boolean isPerfectSquare(int num) {
         long r = num;
         long l = 0;
-        while(l<=r)
+        while(r>=l)
         {
             long mid = l + (r - l)/2;
-            if(mid*mid==num)
+            if(mid*mid == num)
             {
                 return true;
             }
-            else if(mid*mid < num)
-            {
-                l = mid + 1;
-            }
-            else
+            else if (mid*mid > num)
             {
                 r = mid - 1;
             }
-        }
+            else
+            {
+                l = mid + 1;
+            }
+        } 
         return false;
     }
 }
